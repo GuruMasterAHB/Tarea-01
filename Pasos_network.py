aabd30a2ac983ab309158806666f4654576255a3
GuruMasterAHB <juliiooarmandhoo@gmail.com>
2023-09-04 22:40:04 -0600

    Después de varios intentos llegué a notar que si bien se pueden llegar a obtener mejores resultados con más neuronas, depende mucho del porcentaje de las primera épocas. Se puede clasificar en dos casos: 1) si empieza con un porcentaje de menos del 50% solo aumenta hasta un 85%, aproximadamente. 2) si empieza por 70% - 80% aumenta hasta 96%

---
+++ Pasos_network.py
@@ -0,0 +1,35 @@
+'''
+Se importa el dataset de MNIST
+'''
+import mnist_loader
+training_data, validation_data, test_data = \
+mnist_loader.load_data_wrapper()
+
+'''
+Se importa la red:
+    784 neuronas de entrada
+    30 neuronas capa de en medio
+    10 neuronas de salida
+'''
+import network
+net = network.Network([784, 30, 10])
+
+'''
+SGD:
+    30 épocas
+    10 mini-batch size
+    3.0 tasa de aprendizaje
+'''
+net.SGD(training_data, 30, 10, 3.0, test_data=test_data)
+
+# Mejora del resultado aumentando la cantidad de neuronas en la capa de en medio
+net = network.Network([784, 100, 10])
+net.SGD(training_data, 30, 10, 3.0, test_data=test_data)
+
+# 0.001 tasa de aprendizaje
+net = network.Network([784, 100, 10])
+net.SGD(training_data, 30, 10, 0.001, test_data=test_data)
+
+# 100.0 tasa de aprendizaje
+net = network.Network([784, 30, 10])
+net.SGD(training_data, 30, 10, 100.0, test_data=test_data)
