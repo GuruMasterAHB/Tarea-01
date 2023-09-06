515d642b30ac519c587b487a48ad15bb988377ec
GuruMasterAHB <juliiooarmandhoo@gmail.com>
2023-09-05 20:54:51 -0600

    Hay que comentar o descomentar net y net.SGD según sea el interés Mejor así que escribir comando por comando en el shell de python

--- Pasos_network.py
+++ Pasos_network.py
@@ -1,9 +1,8 @@
 '''
 Se importa el dataset de MNIST
 '''
-import mnist_loader
-training_data, validation_data, test_data = \
-mnist_loader.load_data_wrapper()
+import mnist_loader # llama al módulo mnist_loader que desempaqueta el dataset y transforma las imágenes de 28*28 en matrices de 784*1
+training_data, validation_data, test_data = mnist_loader.load_data_wrapper()
 
 '''
 Se importa la red:
